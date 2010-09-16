6f4416b13dd899b371a9a6c382f5e26a7d1065d1
have.you.g0tmi1k <have.you.g0tmi1k@83651c10-56a2-dac7-1390-c40be1c8068d>
2010-09-16 18:48:24 +0000

    Added 'manual' input Fixed 'update' issue Improved internal workings

--- trunk/chap2asleap/chap2asleap.py
+++ trunk/chap2asleap/chap2asleap.py
@@ -1,52 +1,66 @@
 #!/usr/bin/python
 #----------------------------------------------------------------------------------------------#
-#chap2asleap.py v0.2 (2010-09-16)                                                              #
+#chap2asleap.py v0.2.1 (2010-09-16)                                                              #
 # (C)opyright 2010 - g0tmi1k                                                                   #
 #---Important----------------------------------------------------------------------------------#
 #                     *** Do NOT use this for illegal or malicious use ***                     #
 #---Modules------------------------------------------------------------------------------------#
 import os, re, sys, hashlib, getopt, binascii
 
-#---Variables----------------------------------------------------------------------------------#
-asleap_path   = '/pentest/wireless/asleap' # NO trailing '/'
-wordlist_path = '/pentest/passwords/wordlists/darkc0de.lst'
+#---Defaults-----------------------------------------------------------------------------------#
+# [/path/to/the/file] Use which file
+wordlistPath = "/pentest/passwords/wordlists/darkc0de.lst"
+
+# [/path/to/the/folder] Where is asleap?
+asleapPath = "/pentest/wireless/asleap"
+
+# [True/False] Shows more info
 verbose = False
+
+# [True/False] Runs asleap afterwords
 run = False
+
+# [True/False] Use the wordlist for the attack
 wordlist = False
-txtUser = ''
-txtChal = ''
-txtChal = ''
+
+#---Variables----------------------------------------------------------------------------------#
+version = "0.2.1"
+txtUser = "" # null the value
+txtChal = "" # null the value
+txtResp = "" # null the value
 
 #----Functions---------------------------------------------------------------------------------#
 def SplitList( list, chunk_size ):
-	return "".join([list[offs:offs+chunk_size] + ':' for offs in range(0, len(list), chunk_size)])
+	return "".join([list[offs:offs+chunk_size] + ":" for offs in range(0, len(list), chunk_size)])
+
 def help_message():
-	print '''(C)opyright 2010 g0tmi1k ~ http://g0tmi1k.blogspot.com
+	print """(C)opyright 2010 g0tmi1k ~ http://g0tmi1k.blogspot.com
 	
  Usage: python chap2asleap.py [options]
  
  Options:
-   -u username...             -- Username
-   -c 0123456789ABCDEF...     -- PPP CHAP Challenge (32 characters)
-   -r 0123456789ABCDEF...     -- PPP CHAP Response  (98 characters)
+   -u username...            -- Username
+   -c 0123456789ABCDEF...    -- PPP CHAP Challenge (32 characters)
+   -r 0123456789ABCDEF...    -- PPP CHAP Response  (98 characters)
 
-   -h                         -- Displays this help message
-   -v                         -- Verbosity mode (shows more detail)
-
-   -x                         -- Runs asleap with arguments afterwards
-   -w                         -- Uses "Wordlist" instead of "genkey" (Default)
-   -p "/path/to/asleap"       -- Default:''' + asleap_path + '''
-   -d "/path/to/wordlist.lst" -- Default:''' + wordlist_path + '''
+   -x                        -- Runs asleap afterwards
+   -w                        -- Uses "Wordlist" for the attack, instead of "genkey" (Default is genkey)
+   -p /path/to/asleap        -- Example: """ + asleapPath + """
+   -d /path/to/wordlist.lst  -- Example: """ + wordlistPath + """
    
-   --update                   -- Downloads the latest version
+   
+   -h                        -- Displays this help message
+   -v                        -- Verbosity mode (shows more detail)
+   
+   --update                  -- Downloads the latest version
 
  Example:
    python chap2asleap.py -u scott -c e3a5d0775370bda51e16219a06b0278f -r 84c4b33e00d9231645598acf91c384800000000000000000565fe2492fd5fb88edaec934c00d282c046227406c31609b00 -x -v
 
  Extra Help:
-Authors Page: http://www.willhackforsushi.com/Asleap.html
-   Blog Post: http://g0tmi1k.blogspot.com/2010/03/script-chap2asleappy.html
-       Video: http://g0tmi1k.blogspot.com/2010/03/video-cracking-vpn-asleap-thc-pptp.html'''
+   Authors Page: http://www.willhackforsushi.com/Asleap.html
+      Blog Post: http://g0tmi1k.blogspot.com/2010/03/script-chap2asleappy.html
+          Video: http://g0tmi1k.blogspot.com/2010/03/video-cracking-vpn-asleap-thc-pptp.html"""
 	sys.exit(0)
 
 def updateScript():
@@ -57,7 +71,7 @@ def updateScript():
 	req = Request(url)
 	try:
 		f = urlopen(req)
-		local_file = open("chap2asleappy.py", "w")
+		local_file = open("chap2asleap.py", "w")
 		local_file.write(f.read())
 		local_file.close()
 		print "[>] Updated! (="
@@ -73,14 +87,14 @@ print "[*] chap2asleap v0.2 ~ Asleap Argument Generator"
 
 #----------------------------------------------------------------------------------------------#
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "u:c:r:vxwp:d:h", ["user=","challenge=","response=","path=","wordlist=","help","update"])
+    opts, args = getopt.getopt(sys.argv[1:], "u:c:r:vxwp:d:h?", ["user=","challenge=","response=","path=","wordlist=","help", "update"])
 except getopt.GetoptError, err:
     # print help information and exit:
     print str(err) # will print something like "option -a not recognized"
     sys.exit(0)
 
-if len(opts) == 0 :
-    help_message()
+#if len(opts) == 0 :
+#    help_message()
 for o, a in opts:
     if o in ("-u", "--user"):
         txtUser = a
@@ -95,43 +109,52 @@ for o, a in opts:
     if o == "-w":
         wordlist = True
     if o in ("-p", "--path"):
-        asleap_path = a
+        asleapPath = a
     if o in ("-d", "--wordlist"):
-        wordlist_path = a
-    if o in ("-h", "--help"):
+        wordlistPath = a
+    if o in ("-h", "--help", "-?"):
         help_message()
-    if o in ("--update"):
+    if o  == "--update":
         updateScript()
 
 #----------------------------------------------------------------------------------------------#
-if txtUser == "":
-	print "Sorry, you need to input a username (-u)."
-	sys.exit(0)
-if txtChal == "":
-	print "Sorry, you need to input a PPP CHAP Challenge (-c)."
-	sys.exit(0)
-if txtResp == "":
-	print "Sorry, you need to input a PPP CHAP Response (-r)."
-	sys.exit(0)
-txtChal = txtChal.replace(':', '')
-txtResp = txtResp.replace(':', '')
-if len(txtChal) != 32:
-	print "Sorry, PPP CHAP Challenge has to be 32 btyes in length."
-	sys.exit(0)
-if len(txtResp) != 98:
-	print "Sorry, PPP CHAP Response has to be 98 btyes in length."
-	sys.exit(0)
-if not re.search("[0-f]", txtChal):
-	print "Sorry, you cant input that for the CHAP Challenge. 0-9 a-f."
-	sys.exit(0)
-if not re.search("[0-f]", txtResp):
-	print "Sorry, you cant input that for the CHAP Response. 0-9 a-f."
-	sys.exit(0)
+mainLoop = True
+while mainLoop:
+   if txtUser == "" : txtUser = raw_input("Please enter the username: ")
+   else : mainLoop = False
+   
+mainLoop = True
+while mainLoop:
+   if txtChal == "" : txtChal = raw_input("Please enter the PPP CHAP Challenge: ")
+   txtChal = txtChal.replace(":", "")
+   if not re.search("[0-f]", txtChal) :
+	  txtChal = ""
+	  print "Sorry, you can't input that for the CHAP Challenge. Only 0-9 a-f."
+   elif len(txtChal) != 32 :
+	  txtChal = ""
+	  print "Sorry, PPP CHAP Challenge has to be 32 bytes in length."
+   else : 
+      mainLoop = False
+	  
+mainLoop = True
+while mainLoop:
+   if txtResp == "" : txtResp = raw_input("Please enter the PPP CHAP Response: ")
+   txtResp = txtResp.replace(":", "")
+   if not re.search("[0-f]", txtResp) :
+	  print "Sorry, you can't input that for the CHAP Response. Only 0-9 a-f."
+	  txtResp = ""
+   elif len(txtResp) != 98 :
+	  print "Sorry, PPP CHAP Response has to be 32 bytes in length."
+	  txtResp = ""
+   else : 
+      mainLoop = False
+
+if asleapPath[-1:] == "/" : asleapPath = asleapPath[0:-1]
 	
 #----------------------------------------------------------------------------------------------#
-if verbose == True: print "[>]       Username: " + txtUser
-if verbose == True: print "[>] CHAP Challenge: " + txtChal
-if verbose == True: print "[>]  CHAP Response: " + txtResp
+if verbose == True: print "[i]       Username: " + txtUser
+if verbose == True: print "[i] CHAP Challenge: " + txtChal
+if verbose == True: print "[i]  CHAP Response: " + txtResp
 
 #----------------------------------------------------------------------------------------------#
 authChallenge = binascii.unhexlify(txtChal)
@@ -141,25 +164,32 @@ response = txtResp[48:96]
 
 challenge = ((hashlib.sha1( peerChallenge + authChallenge + txtUser )).hexdigest())[0:16]
 
-if verbose == True: print "[>] Auth Challenge: " + txtChal
-if verbose == True: print "[>] Peer Challenge: " + (txtResp)[0:32]
-if verbose == True: print "[>]  Peer Response: " + response
-if verbose == True: print "[>]      Challenge: " + challenge + "\n"
+if verbose == True: print "[i] Auth Challenge: " + txtChal
+if verbose == True: print "[i] Peer Challenge: " + (txtResp)[0:32]
+if verbose == True: print "[i]  Peer Response: " + response
+if verbose == True: print "[i]      Challenge: " + challenge
 
 challenge = (SplitList (challenge,2 ))[0:-1]
-response  = (SplitList  (response,2 ))[0:-1]
+response  = (SplitList (response,2 ))[0:-1]
 
 #----------------------------------------------------------------------------------------------#
-if verbose == True: print 'cd '+ asleap_path
-if (verbose == True and wordlist == False): print './genkey -r ' + wordlist_path + ' -f words.dat -n words.idx'
-if wordlist == False: print './asleap -C ' + challenge + ' -R ' + response + ' -f words.dat -n words.idx' + '\n\n'
-if wordlist == True: print './asleap -C ' + challenge + ' -R ' + response + ' -W ' + wordlist_path + '\n\n'
+if verbose == True : 
+   print "cd " + asleapPath
+   if wordlist == False : 
+      print "./genkey -r " + wordlistPath + " -f words.dat -n words.idx"
+      print "./asleap -C " + challenge + " -R " + response + " -f words.dat -n words.idx"
+   else :
+      print "./asleap -C " + challenge + " -R " + response + " -W " + wordlistPath
 
 #----------------------------------------------------------------------------------------------#
-if (run == True and wordlist == False): os.system( asleap_path + '/genkeys -r ' + wordlist_path + ' -f /tmp/words.dat -n /tmp/words.idx')
-if (run == True and wordlist == False): os.system( asleap_path + '/asleap -C ' + challenge + ' -R ' + response + ' -f /tmp/words.dat -n /tmp/words.idx')
-if (run == True and wordlist == True): os.system( asleap_path + '/asleap -C ' + challenge + ' -R ' + response + ' -W ' + wordlist_path)
-
-#----------------------------------------------------------------------------------------------#
-if (run == True and wordlist == False): os.remove ('/tmp/words.dat')
-if (run == True and wordlist == False): os.remove ('/tmp/words.idx')
+if (os.path.isfile(asleapPath + "/genkeys") and run == True) :
+   if wordlist == False : 
+      os.system( asleapPath + "/genkeys -r " + wordlistPath + " -f /tmp/words.dat -n /tmp/words.idx")
+      os.system( asleapPath + "/asleap -C " + challenge + " -R " + response + " -f /tmp/words.dat -n /tmp/words.idx")
+      os.remove ("/tmp/words.dat")
+      os.remove ("/tmp/words.idx")
+   if wordlist == True : 
+      os.system( asleapPath + "/asleap -C " + challenge + " -R " + response + " -W " + wordlistPath)
+elif run == True :
+   print "alseap isn't located: " + asleapPath
+   
